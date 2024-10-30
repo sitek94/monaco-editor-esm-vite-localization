af0f9aa1a575d95d0ba0b71ac9285a203de331c6
Maciek Sitkowski <msitkowski94@gmail.com>
2024-10-30 21:54:56 +0100

    create vite plugin to modify source code of our app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import {type Plugin} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), helloWorldPlugin()],
 })
+
+function helloWorldPlugin(): Plugin {
+  return {
+    name: 'Hello World Vite Plugin',
+    buildStart() {
+      // console.log('[Hello World Vite Plugin]: 🚀🚀🚀')
+    },
+    transform(code, id) {
+      if (id.endsWith('/app.tsx')) {
+        return code.replace(/__APP_TITLE__/g, 'Monaco Editor')
+      }
+
+      return null
+    },
+  }
+}
